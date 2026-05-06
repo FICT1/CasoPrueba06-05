40eb5056c9c923da0e74540be576aa6c98a77ac9
FICT1 <isaquitopro10@gmail.com>
2026-05-06 15:59:58 -0600

    Clase Cliente

--- CasoPrueba/src/main/java/ni/edu/uam/modelos/Cliente.java
+++ CasoPrueba/src/main/java/ni/edu/uam/modelos/Cliente.java
@@ -1,5 +1,17 @@
 package ni.edu.uam.modelos;
-
 public class Cliente {
+    private String nombre;
+    public Cliente() {
 
+    }
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public Pedido realizarPedido() {
+        System.out.println(nombre + " está realizando un pedido...");
+        return new Pedido();
+    }
 }
