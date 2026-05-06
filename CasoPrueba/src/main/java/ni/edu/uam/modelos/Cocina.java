bfbb7422479f2202b3be17671c2e86f1003c6092
FICT1 <isaquitopro10@gmail.com>
2026-05-06 15:58:57 -0600

    Clase Cocina

--- CasoPrueba/src/main/java/ni/edu/uam/modelos/Cocina.java
+++ CasoPrueba/src/main/java/ni/edu/uam/modelos/Cocina.java
@@ -2,4 +2,8 @@ package ni.edu.uam.modelos;
 
 public class Cocina {
 
+    public void procesarPedido(Pedido p) {
+        System.out.println("Cocina está preparando el pedido...");
+        System.out.println(p.mostrarPedido());
+    }
 }
