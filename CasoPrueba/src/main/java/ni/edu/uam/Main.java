05ae1e624770578362d738f07735e1da2604b4c7
FICT1 <isaquitopro10@gmail.com>
2026-05-06 16:02:32 -0600

    Main terminado

--- CasoPrueba/src/main/java/ni/edu/uam/Main.java
+++ CasoPrueba/src/main/java/ni/edu/uam/Main.java
@@ -1,5 +1,10 @@
 package ni.edu.uam;
+
+import ni.edu.uam.interfaz.Menu;
+
 public class Main {
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.iniciar();
+    }
 }
