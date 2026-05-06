18cd6cdb145f0a4eac301d748e67b05c948c523e
FICT1 <isaquitopro10@gmail.com>
2026-05-06 16:00:37 -0600

    Clase Cajero

--- CasoPrueba/src/main/java/ni/edu/uam/modelos/Cajero.java
+++ CasoPrueba/src/main/java/ni/edu/uam/modelos/Cajero.java
@@ -1,5 +1,25 @@
 package ni.edu.uam.modelos;
-
 public class Cajero {
+    private String nombre;
 
+    public Cajero() {}
+
+    public Cajero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void registrarPedido(Pedido p) {
+        System.out.println("Cajero " + nombre + " registró el pedido");
+        System.out.println(p.mostrarPedido());
+    }
+
+    
+    public void enviarACocina(Pedido p, Cocina cocina) {
+        System.out.println("Cajero envía pedido a cocina...");
+        cocina.procesarPedido(p);
+    }
+
+    public void notificarCliente(Cliente c) {
+        System.out.println("Pedido listo, notificando a " + c.getNombre());
+    }
 }
